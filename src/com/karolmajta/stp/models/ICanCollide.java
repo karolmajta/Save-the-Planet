f7e20574a701b12fec51508e73d7ad893c4139b4
lolek09 <karolmajta@gmail.com>
2011-12-30 23:10:59 +0100

    Docs added

--- src/com/karolmajta/stp/models/ICanCollide.java
+++ src/com/karolmajta/stp/models/ICanCollide.java
@@ -16,7 +16,16 @@ public interface ICanCollide<With> {
 	 * affect the state of this, not other. This way this can be collided with
 	 * any object (even one that does not implement ICanCollide).
 	 * A call to collide should modify the state of this basing on the state
-	 * of other.
+	 * of this and other.
+	 * 
+	 * Usually the usage of this method will look something like this.
+	 * 
+	 * <code>
+	 * FirstCanCollide a;
+	 * SecondCanCollide b;
+	 * a.collide(b) // affects only state of a basing on a and b
+	 * b.collide(a) // affects only state of b basing on a and b
+	 * </code>
 	 * 
 	 * @param other object that collides with this.
 	 */
